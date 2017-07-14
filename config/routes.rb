9b36c27d279702107eb32e7771aeda8d4929ab10
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-07-14 13:19:43 +0900

    Add Edit View

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
 root'users#index'
  resources :themes, only: [:index, :new, :create]
- resources :answers, only: [:index]
+ resources :answers, only: [:index, :new]
  resources :users, only: [:index, :show]
+  resources :photo, only: [:index]
 
 end
