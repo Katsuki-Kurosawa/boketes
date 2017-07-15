5c13bcf0cbc9a9064e3b50e7578071b7ba980c78
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-07-16 02:09:14 +0900

    Edit application.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,10 @@ class UsersController < ApplicationController
    @answer = Answer.all.order("created_at   DESC").limit(6)
  end
 
-
-  def show
-    @user = current_user
-    @theme = @user.themes.all.order("created_at   DESC").limit(3)
-    @answer = @user.answers.all.order("created_at   DESC").limit(3)
-  end
+ def show
+  @user = current_user
+  @theme = @user.themes.all.order("created_at   DESC").limit(3)
+  @answer = @user.answers.all.order("created_at   DESC").limit(3)
+end
 
 end
