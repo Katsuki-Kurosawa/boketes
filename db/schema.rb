afc64af8285bf087d51138995e53a8cd365b8428
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-07-22 14:25:27 +0900

    User addカラム　month

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170722052251) do
+ActiveRecord::Schema.define(version: 20170722052451) do
 
   create_table "answers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "answer",     limit: 65535
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20170722052251) do
     t.string   "sex"
     t.text     "text",                   limit: 65535
     t.integer  "year"
+    t.integer  "month"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
