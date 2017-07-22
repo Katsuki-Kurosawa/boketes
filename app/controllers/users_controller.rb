9d22821e37291377a9367ed97f6d26243418ff7b
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-07-22 19:13:47 +0900

    Edit user の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,9 @@ class UsersController < ApplicationController
    @answer = Answer.all.order("created_at   DESC").limit(6)
    @answers = Answer.all.order("created_at   ASC").limit(6)
    @bad_count = Answer.group(:user_id).order('sum_bads_count DESC').limit(3).sum(:bads_count)
+ end
 
+ def edit
  end
 
  def show
