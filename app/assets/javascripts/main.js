876f7aab01e14378ed5695fca81146dd85c0500d
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-07-30 12:53:28 +0900

    Edit app/views/layouts/application.html.erb

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,24 @@
+$(function(){
+  $('#star1').on("click",function(e){
+    e.preventDefault();
+      console.log(this);
+  });
+});
+
+     $(function() {
+              function readURL(input) {
+                if (input.files && input.files[0]) {
+                  var reader = new FileReader();
+
+                  reader.onload = function (e) {
+                    $('#img_prev').attr('src', e.target.result);
+                  }
+                  reader.readAsDataURL(input.files[0]);
+                }
+              }
+
+              $(".a").change(function(){
+                $('#img_prev').removeClass('hidden');
+                readURL(this);
+              });
+            });
