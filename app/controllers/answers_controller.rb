e3fb4fe7ff55a492652df52fbcdbe319b8ef150e
Katsuki-Kurosawa <kurosawakatsuki@kurosawakatsunorinoMacBook-Pro.local>
2017-08-04 16:30:38 +0900

    Edit answers_controller.rb

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,5 @@
 class AnswersController < ApplicationController
 
-
-
   def index
     @answer = Answer.all.order("created_at  DESC").limit(20)
     @bad = current_user.bads
@@ -17,16 +15,15 @@ class AnswersController < ApplicationController
 
   def create
    @answer = Answer.new(answer_params)
-   if @answer.save
-    redirect_to root_path
+    if @answer.save
+     redirect_to root_path
+    end
   end
-end
 
+  private
 
-private
-
-def answer_params
-  params.permit(:answer,:theme_id).merge(user_id: current_user.id)
-end
+  def answer_params
+    params.permit(:answer,:theme_id).merge(user_id: current_user.id)
+  end
 
 end
